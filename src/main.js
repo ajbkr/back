8a6e40fe03b4ad0ea1685d9bed9877c4edc76b7b
Andrew Baker <andrew_j_baker2@hotmail.com>
2019-08-21 16:45:18 +0100

    Resize canvas on window resize, maintaining aspect ratio

--- src/main.js
+++ src/main.js
@@ -223,7 +223,24 @@ function movePlayerSouth () {
   }
 }
 
+function resizeCanvas () {
+  const { innerWidth, innerHeight } = window
+  const windowRatio = innerWidth / innerHeight
+  const canvasRatio = SCREEN_WIDTH / SCREEN_HEIGHT
+
+  if (windowRatio < canvasRatio) {
+    canvas.style.width = innerWidth + 'px'
+    canvas.style.height = (innerWidth / canvasRatio) + 'px'
+  } else {
+    canvas.style.width = (innerHeight * canvasRatio) + 'px'
+    canvas.style.height = innerHeight + 'px'
+  }
+}
+
 function main () {
+  resizeCanvas()
+  window.addEventListener('resize', resizeCanvas)
+
   const loop = GameLoop({
     render () {
       context.fillStyle = palette[c['black']]
